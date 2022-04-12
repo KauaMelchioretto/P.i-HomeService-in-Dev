7f9f0d4cec8702408aaf504402322fdfdceebb5f
unknown <kauamelchoretto@gmail.com>
2022-04-12 10:37:56 -0300

    tevao commit

--- client/src/pages/RegisterScreen.jsx
+++ client/src/pages/RegisterScreen.jsx
@@ -7,7 +7,6 @@ export default function RegisterScreen() {
             <div>
             <div className="Registro">
                 <h1 className='title'>Home Service</h1>
-                <h2>Registre aqui seu serviço!</h2>
             </div>
 
             <section className="register--section">
@@ -22,7 +21,11 @@ export default function RegisterScreen() {
                 </div>
                 <div className='box-register'>
                     <label>Cidade de atuação</label>
-                    <input id="cidade" name="cidade" placeholder="Digite a cidade de atuação" required="Text" />
+                    <input id="cidade" name="cidade" placeholder="Principal" required="Text" />
+                </div>
+                <div className='box-register'>
+                    <label>Cidade de atuação</label>
+                    <input id="cidade" name="cidade" placeholder="Secundaria (opcional)" required="Text" />
                 </div>
                 <div className='box-register'>
                     <label>Número de telefone</label>
@@ -30,7 +33,7 @@ export default function RegisterScreen() {
                 </div>
                 <div className='box-register'>
                     <label>Descrição</label>
-                    <textarea rows="10" name="desc"  required="text" />
+                    <textarea rows="8" name="desc"  required="text" />
                 </div>
             </form>
                 
@@ -38,7 +41,7 @@ export default function RegisterScreen() {
                     <button type="submit">Salvar</button>
                 </div> 
                 <div className="div-buttons-descartar">    
-                    <button type="submit">Descartar</button>
+                    <button type="reset">Descartar</button>
                 </div>
             
             </section>
