faece11932f26de06865e293b7442866a37719df
Kauã Melchioretto <kauamelchoretto@gmail.com>
2022-04-12 19:30:06 -0300

    kaua commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
 
-app.get("/", (request, result) => {
-    result.send("Hello worlasdasd");
-}); 
+const dataBase = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "senha",
+    database: "crudservices",
+});
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(3001, () => {
     console.log("rodando server");
